5e3e1b27ac36f00cfa051d4336ce7f667e537588
sstokes <sterlingjstokes@gmail.com>
2024-12-12 12:01:53 -0500

    Push

--- index.js
+++ index.js
@@ -5,17 +5,67 @@ const port = 3000;
 const users = require('./data/users');
 const posts = require('./data/posts');
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({extended: true}))
+
+
 //Create our first route
 app.get('/', (req,res) => {
     res.send("Work in progress");
 })
+app.get('/api/users', (req,res) => {
+    res.json(users);
+})
 
+app.post('/api/users',(req,res) => {
+    if(req.body.name && req.body.username && req.body.email){
+        if(users.find((u) => u.username == req.body.username)){
+            res.json({error: "Username Already Taken"}); //! Send error name as Json
+            return  //! Do Nothing
+        }
 
+        const user = {
+            id: users[users.length - 1].id + 1, //! Create on unique issubtract one to account for zero based indexing
+            name: req.body.name,
+            username: req.body.username,
+            email: req.body.email
+        }
+
+        users.push(user);
+        res.json(users[users.length - 1]);
+    }else res.json({error:"Insufficent Data"});
+})
 app.get('/api/users/:id', (req, res, next)=>{
     const user = users.find((u)=> u.id == req.params.id);
     if(user) res.json(user);
     else next()
 });
+app.patch('/api/users/:id', (req,res) => {
+    const user = users.find((u,i)=>{
+        if(u.id == req.params.id){
+            for(const key in req.body){
+                users[i][key] = req.body[key]
+            }
+        }
+        return true
+    })
+    if(user)
+        res.json(user);
+    else
+        next();
+})
+//removes a user
+app.delete('/api/users/:id', (req,res) => {
+    const user = users.find((u,i) => {
+        if(u.id == req.params.id){
+            users.splice(i, 1);
+            return true 
+        }
+    })
+    if(user) res.json(user);
+})
 
 app.get('/api/posts', (req,res)=>{
     res.json(posts);
@@ -26,6 +76,10 @@ app.get('api/posts/:id', (req, res, next) =>{
     if(post) res.json(post);
     else next()
  })
+
+
+
+
 
 // Middleware at the end of the route
 app.use((req,res)=>{
